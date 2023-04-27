80463c6a2f1721a06b3bab7c44a98d4b195b931e
m00nlex <you@example.com>
2023-04-27 15:07:57 +0500

    visual form of app without js

---
+++ CLIENT/DB CARDS/src/components/CardHolder.jsx
@@ -0,0 +1,19 @@
+// // import { useState } from 'react'
+import '.././App.css'
+
+function CardHolder() {
+    return (
+        <div  className="cardHolder">
+            <div className="card">heh</div>
+            <div className="card">huh</div>
+            <div className="card">hah</div>
+            <div className="card">hih</div><div className="card">heh</div>
+            <div className="card">huh</div>
+            <div className="card">hah</div>
+            <div className="card">hih</div>
+            <div className="card addCard">+</div>
+        </div>
+    )
+}
+
+export default CardHolder
